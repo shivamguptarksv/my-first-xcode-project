cce26bc93cf812df9522aa0dd067600d67f0e719
shivamguptarksv <shivam.gupta@rksv.in>
2022-01-12 14:55:39 +0530

    Initial Commit

---
+++ my first xcode project/my_first_xcode_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_xcode_projectApp.swift
+//  my first xcode project
+//
+//  Created by upstox on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct my_first_xcode_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
